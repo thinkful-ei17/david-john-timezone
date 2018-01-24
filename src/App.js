262bf00393f790ec623992819919d83bc0621c69
John Castillo <JohnluisCastillo@gmail.com>
2018-01-24 11:19:42 -0500

    some comp

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
+import React from 'react';
+import Input from './Input'
+import Selection from './Selection'
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeInput: ''
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Time Zone Converter</h1>
+          <Input onChange={e => this.setState({timeInput: e})}/>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Selection/>
       </div>
     );
   }
---
+++ src/Selection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Selection = (props) => {
+    return (
+        <div>
+            <select>
+                <option>EST</option>
+                <option>PST</option>
+                <option>MST</option>
+                <option>CST</option>
+            </select>
+        </div>
+    );
+};
+
+export default Selection;
